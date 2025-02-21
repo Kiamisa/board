d15b69d647b43b660f1193330578b752abde8711
Kiamisa <silas10henriique@gmail.com>
2025-02-21 11:49:29 -0300

    Setup de migrations

--- src/main/resources/db/changelog/migrations/db.changelog-202502191223.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202502191223.sql
@@ -1,12 +1,10 @@
 --liquibase formatted sql
-
---changeset silas:001
+--changeset silas:202501191223
 --comment: board table create
-USE board;
 
 CREATE TABLE BOARDS (
                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(255) NOT NULL
 ) ENGINE = InnoDB;
 
---rollback DROP TABLE BOARDS;
+--rollback DROP TABLE BOARDS
